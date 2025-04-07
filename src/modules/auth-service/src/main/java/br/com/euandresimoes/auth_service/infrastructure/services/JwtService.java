21c8b386a77dfa160d56a1131bd9b05c8a780a0f
André <andre.simoesdutra23@gmail.com>
2025-04-07 02:24:40 -0300

    chore(backend/auth-service/application.yml): minor config adjustments

--- src/modules/auth-service/src/main/java/br/com/euandresimoes/auth_service/infrastructure/services/JwtService.java
+++ src/modules/auth-service/src/main/java/br/com/euandresimoes/auth_service/infrastructure/services/JwtService.java
@@ -6,10 +6,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+@Service
 public class JwtService implements IJwtService {
 
     @Value("${JWT.SECRET}")
